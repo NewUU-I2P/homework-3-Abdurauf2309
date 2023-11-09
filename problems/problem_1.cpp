0d2a5d4978d7e4bfceaa375d75cc7bce09402654
Abdurauf2309 <147691247+Abdurauf2309@users.noreply.github.com>
2023-11-09 16:52:45 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -8,7 +8,7 @@ float problemSolution1(float consumed_water) {
     }else if (consumed_water<=60) {
         cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
     }else {
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10*1.4+(consumed_water - 60) * 1.5;
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 +( consumed_water - 60) * 1.5;
     }
 
     return cost;
