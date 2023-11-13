781f7768e6cc941b3430181ac0c57f0b448eeeb2
Abdurauf2309 <a.qarshiboyev@newuu.uz>
2023-11-13 23:57:19 +0500

    problem 1, 2, 3, 4, 5 of Homework 3 are done!

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,24 +1,18 @@
 #include <string>
 #include <sstream>
-#include <iomanip>
 using namespace std;
-std::string problemSolution4(const std::string &macAddress) {
-    std::string result;
-    // write your code here
-        stringstream ss(macAddress);
-        int firstOctet;
-        char colon;
-        ss >> hex >> firstOctet >> colon;
-        if (ss && firstOctet == 0xFF) {
-            result =  "Broadcast";
-        }
-        if (ss && firstOctet % 2 == 0) {
-            result = "Unicast";
-        } else if (ss) {
-            result = "Multicast";
-        }
-    result = "Invalid";
-    // make use of control flow statements
-     return result;
-}
 
+string problemSolution4(const string &macAddress) {
+    string result;
+    int firstOct;
+    stringstream sso(macAddress);
+    sso >> hex >> firstOct;
+    if (macAddress=="FF:FF:FF:FF:FF:FF"){
+        result = "Broadcast";
+    }
+    if (firstOct%2==0){
+        result = "Unicast";
+    }else
+        result = "Multicast";
+    return result;
+}
